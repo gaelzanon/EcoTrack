cc22e4db1836847bdc8bebac7e832324807e924d
danielgauchia <pepehhhhhh@gmail.com>
2023-11-20 11:21:11 +0100

    refactor(it01h02): valida mail y pass en login

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -2,7 +2,16 @@ class UserController {
   constructor(authService) {
     this.authService = authService;
   }
+  validarEmail(email) {
+    const regex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/;
+    return regex.test(email);
+  }
+  validarPassword(password) {
+    const longitudValida = password.length >= 6;
+    const tieneDosNumeros = (password.match(/\d/g) || []).length >= 2;
 
+    return longitudValida && tieneDosNumeros;
+  }
   async register(user) {
     // Valida la contraseña antes de intentar registrar al usuario
     
@@ -20,7 +29,12 @@ class UserController {
 
   async login(user) {
     // Valida la contraseña antes de intentar loggear al usuario
-    
+    if (!this.validarEmail(user.email)) {
+      throw new Error('InvalidEmailException')
+    }
+    if (!this.validarPassword(user.password)) {
+      throw new Error('InvalidPasswordException')
+    }
     try {
       const result = await this.authService.signInWithEmailAndPassword(
         user.email,
