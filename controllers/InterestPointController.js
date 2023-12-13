e57cb6299d45e1e8433267e44650cc41eed5b326
danielgauchia <pepehhhhhh@gmail.com>
2023-12-13 13:32:05 +0100

    fix

--- controllers/InterestPointController.js
+++ controllers/InterestPointController.js
@@ -51,6 +51,7 @@ class InterestPointController {
   async getInterestPoints() {
     const list = await this.cloudService.getInterestPoints();
     return list;
+  }
     
   async removeInterestPoint(interestPoint) {
     return 0
